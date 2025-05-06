475378d63d316b61c0b572e0cf4fe9d67099c689
Leon <bobroleon@gmail.com>
2025-05-06 12:45:24 +0700

    Создал интеллектуальную фикстуру с проверкой валидности

--- test/test_add_group.py
+++ test/test_add_group.py
@@ -3,11 +3,9 @@
 from model.group import Group
 
 def test_add_group(app):
-    app.session.login(username="admin", password="secret")
     app.group.create(Group("First", "first_group", "group_footer"))
-    app.session.logout()
+
 
 def test_add_empty_group(app):
-    app.session.login(username="admin", password="secret")
     app.group.create(Group("", "", ""))
-    app.session.logout()
+
